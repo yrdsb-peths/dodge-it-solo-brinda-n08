7de66818b1f7051250865095dd49dec9034e96d2
brinda-n08 <349035105@gapps.yrdsb.ca>
2025-04-17 08:45:32 -0400

    Add banana

---
+++ Banana.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Banana here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Banana extends Actor
+{
+    
+    public void act()
+    {
+       move(-3);     
+    }
+}
